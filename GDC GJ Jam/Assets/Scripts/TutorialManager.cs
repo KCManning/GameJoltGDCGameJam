51f95738bb5394cda9dde2802b68c3c1d0987ef7
JustinWeq <zoomdude111@hotmail.com>
2016-02-05 13:25:16 -0800

    Started working on tutorial level

---
+++ GDC GJ Jam/Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialManager : MonoBehaviour {
+
+    public float movement_distance;
+    public float jump_distance;
+    public float end_distance;
+    public string movement_message;
+    public string jump_message;
+    public string end_message;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	
+	}
+}
