775d99a46e236538f4c58323a291abd2f1d1b7b5
JustinWeq <zoomdude111@hotmail.com>
2016-02-07 18:06:56 -0800

    Added boss level and some polish to levels

---
+++ GDC GJ Jam/Assets/KU_Script.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class KU_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(transform.position.y < -10)
+        {
+            //go back to main menu
+            SceneManager.LoadScene("Credits");
+        }
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        //if the other is a player then kill self
+        if(other.collider.tag != "JumpableSurface")
+        {
+            Animator ani = GetComponent<Animator>();
+            ani.SetFloat("Sand", 0);
+        }
+    }
+}
